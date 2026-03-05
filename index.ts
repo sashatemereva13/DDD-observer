c36977fd5fa701b4dd3cfc055f4047d3eaa61568
Aleksandra Temereva <aleksandra.temereva@epita.fr>
2026-03-05 08:36:47 +0100

    phase2: it runs but the result is wrong

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from "uuid";
 
 /*
  **
@@ -8,20 +8,44 @@ import { v4 as uuidv4 } from "uuid"
 /* 1.  factory function  */
 /*  a factory function creates a new object  */
 
+type Project = {
+  clientName: string;
+  clientEmail: string;
+  price: number;
+  immersive: boolean;
+};
+
+const project1: Project = {
+  clientName: "Yann",
+  clientEmail: "yan@mail.com",
+  price: 1000,
+  immersive: true,
+};
+
+const project2: Project = {
+  clientName: "Ilias",
+  clientEmail: "ilias@mail.com",
+  price: 2000,
+  immersive: true,
+};
+
 /* 2.  constructor function  */
 // OOP
 // a constructor function creates a new object and sets its properties
+
+function calculateTotalRevenue(projects: Project[]): number {
+  return projects.reduce((total, project) => total + project.price, 0);
+}
 
 // create a primitive obsessed type
 
 // modify this code for testing !!
 // this replicates user input
-const orderOne = {
-	id: uuidv4(), // generate a unique id for the order
-	name: "order one",
-	price: -100,
-	quantity: 200000000,
-	total: 456465465465465400,
-}
+const project3: Project = {
+  clientName: "Karan",
+  clientEmail: "karan.,.fihei.com",
+  price: 3000,
+  immersive: false,
+};
 
-console.log(orderOne)
+console.log(orderOne);
